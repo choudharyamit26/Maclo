f7a24d7b7d232eabd450de808a7d4b5766fedc27
choudharyamit26 <ravichoiudhary766@gmail.com>
2021-01-20 18:26:48 +0530

    Added Update distance range and age range

--- src/views.py
+++ src/views.py
@@ -885,12 +885,12 @@ class UserslistAPIView(APIView):
         # for obj in queryset1:
         #     users.append(obj)
         # return Response({"Users":users}, HTTP_200_OK)
-        user_detail_obj = UserDetail.objects.get(phone_number=registr_user.id)
-        lang = user_detail_obj.discovery[0]
-        lat = user_detail_obj.discovery[1]
-        users_location = fromstr('Point({} {})'.format(lat, lang), srid=4326)
-        users_in_range = UserDetail.objects.filter(discovery__distance_lte=(users_location, D(km=10))).exclude(
-            phone_number=registr_user.id)
+        # user_detail_obj = UserDetail.objects.get(phone_number=registr_user.id)
+        # lang = user_detail_obj.discovery[0]
+        # lat = user_detail_obj.discovery[1]
+        # users_location = fromstr('Point({} {})'.format(lat, lang), srid=4326)
+        # users_in_range = UserDetail.objects.filter(discovery__distance_lte=(users_location, D(km=10))).exclude(
+        #     phone_number=registr_user.id)
         if (UserDetail.objects.all().exclude(phone_number=registr_user.id)).count() > 0:
             for obj in (UserDetail.objects.all().exclude(phone_number=registr_user.id)):
                 id = obj.id
