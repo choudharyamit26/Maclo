611f56e122fb03476b63999e24864c8d25511291
choudharyamit26 <ravichoudhary766@gmail.com>
2021-02-05 17:11:32 +0530

    Added chat api's

--- maclo/asgi.py
+++ maclo/asgi.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import get_default_application
+import os
+import django
 
+django.setup()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'maclo.settings')
 
-application = get_asgi_application()
+application = get_default_application()
