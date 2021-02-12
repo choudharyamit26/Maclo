5ab82ab390897fe3d3a87e030d330558779ab480
choudharyamit26 <ravichoudhary766@gmail.com>
2021-02-12 14:21:20 +0530

    Added super liked user in liked users list

--- src/views.py
+++ src/views.py
@@ -2484,7 +2484,92 @@ class FacebookSignupApiView(CreateAPIView):
                 user_with_token = user_with_token[0]
                 print(user_with_token)
                 user_data = RegisterUser.objects.get(email=user.email)
-                return Response({"Token": user_with_token.key, "user_id": user_data.id, "status": HTTP_200_OK})
+                user_detail = UserDetail.objects.get(phone_number=user_data)
+                pic_1 = ''
+                pic_2 = ''
+                pic_3 = ''
+                pic_4 = ''
+                pic_5 = ''
+                pic_6 = ''
+                pic_7 = ''
+                pic_8 = ''
+                pic_9 = ''
+                if user_data.pic_1:
+                    pic_1 = user_data.pic_1.url
+                else:
+                    pic_1 = ''
+                if user_data.pic_2:
+                    pic_2 = user_data.pic_2.url
+                else:
+                    pic_2 = ''
+                if user_data.pic_3:
+                    pic_3 = user_data.pic_3.url
+                else:
+                    pic_3 = ''
+                if user_data.pic_4:
+                    pic_4 = user_data.pic_4.url
+                else:
+                    pic_4 = ''
+                if user_data.pic_5:
+                    pic_5 = user_data.pic_5.url
+                else:
+                    pic_5 = ''
+                if user_data.pic_6:
+                    pic_6 = user_data.pic_6.url
+                else:
+                    pic_6 = ''
+                # if user_data.pic_7:
+                #     pic_7 = user_data.pic_8.url
+                # else:
+                #     pic_7 = ''
+                # if user_data.pic_8:
+                #     pic_8 = user_data.pic_8.url
+                # else:
+                #     pic_8 = ''
+                # if user_data.pic_9:
+                #     pic_9 = user_data.pic_9.url
+                # else:
+                #     pic_9 = ''
+                Data = {
+                    "id": user_data.id,
+                    "email": user_data.email,
+                    "first_name": user_data.first_name,
+                    "last_name": user_data.last_name,
+                    "phone_number": user_data.phone_number,
+                    "gender": user_data.gender,
+                    "date_of_birth": user_data.date_of_birth,
+                    # "job_profile": user_data.job_profile,
+                    # "company_name": user_data.company_name,
+                    # "qualification": user_data.qualification,
+                    # "relationship_status": user_data.relationship_status,
+                    # "interests": user_data.interests,
+                    # "fav_quote": user_data.fav_quote,
+                    "pic_1": pic_1,
+                    "pic_2": pic_2,
+                    "pic_3": pic_3,
+                    "pic_4": pic_4,
+                    "pic_5": pic_5,
+                    "pic_6": pic_6,
+                    # "pic_7": pic_7,
+                    # "pic_8": pic_8,
+                    # "pic_9": pic_9,
+                    "discovery_lat": user_detail.discovery[0],
+                    "discovery_lang": user_detail.discovery[1],
+                    "distance_range": user_detail.distance_range,
+                    "min_age_range": user_detail.min_age_range,
+                    "max_age_range": user_detail.max_age_range,
+                    "interested": user_detail.interest
+                }
+                r_user = RegisterUser.objects.get(email=user.email)
+                account = DeactivateAccount.objects.get(user=r_user)
+                if account.deactivated:
+                    account.deactivated = False
+                    account.save()
+                    user_detail.deactivated = False
+                    user_detail.save()
+                return Response(
+                    {"Token": user_with_token.key, "user_id": user_data.id, 'data': Data,
+                     'deactivated': account.deactivated, "status": HTTP_200_OK})
         except:
             print('inside except')
             print('Email fb except------------>>>', self.request.data)
@@ -2512,7 +2597,93 @@ class FacebookSignupApiView(CreateAPIView):
                     user=reg_usr
                 )
                 token = Token.objects.create(user=user)
-                return Response({"Token": token.key, "user_id": reg_usr.id, "status": HTTP_200_OK})
+                user_data = RegisterUser.objects.get(email=user.email)
+                user_detail = UserDetail.objects.get(phone_number=user_data)
+                pic_1 = ''
+                pic_2 = ''
+                pic_3 = ''
+                pic_4 = ''
+                pic_5 = ''
+                pic_6 = ''
+                pic_7 = ''
+                pic_8 = ''
+                pic_9 = ''
+                if user_data.pic_1:
+                    pic_1 = user_data.pic_1.url
+                else:
+                    pic_1 = ''
+                if user_data.pic_2:
+                    pic_2 = user_data.pic_2.url
+                else:
+                    pic_2 = ''
+                if user_data.pic_3:
+                    pic_3 = user_data.pic_3.url
+                else:
+                    pic_3 = ''
+                if user_data.pic_4:
+                    pic_4 = user_data.pic_4.url
+                else:
+                    pic_4 = ''
+                if user_data.pic_5:
+                    pic_5 = user_data.pic_5.url
+                else:
+                    pic_5 = ''
+                if user_data.pic_6:
+                    pic_6 = user_data.pic_6.url
+                else:
+                    pic_6 = ''
+                # if user_data.pic_7:
+                #     pic_7 = user_data.pic_8.url
+                # else:
+                #     pic_7 = ''
+                # if user_data.pic_8:
+                #     pic_8 = user_data.pic_8.url
+                # else:
+                #     pic_8 = ''
+                # if user_data.pic_9:
+                #     pic_9 = user_data.pic_9.url
+                # else:
+                #     pic_9 = ''
+                Data = {
+                    "id": user_data.id,
+                    "email": user_data.email,
+                    "first_name": user_data.first_name,
+                    "last_name": user_data.last_name,
+                    "phone_number": user_data.phone_number,
+                    "gender": user_data.gender,
+                    "date_of_birth": user_data.date_of_birth,
+                    # "job_profile": user_data.job_profile,
+                    # "company_name": user_data.company_name,
+                    # "qualification": user_data.qualification,
+                    # "relationship_status": user_data.relationship_status,
+                    # "interests": user_data.interests,
+                    # "fav_quote": user_data.fav_quote,
+                    "pic_1": pic_1,
+                    "pic_2": pic_2,
+                    "pic_3": pic_3,
+                    "pic_4": pic_4,
+                    "pic_5": pic_5,
+                    "pic_6": pic_6,
+                    # "pic_7": pic_7,
+                    # "pic_8": pic_8,
+                    # "pic_9": pic_9,
+                    "discovery_lat": user_detail.discovery[0],
+                    "discovery_lang": user_detail.discovery[1],
+                    "distance_range": user_detail.distance_range,
+                    "min_age_range": user_detail.min_age_range,
+                    "max_age_range": user_detail.max_age_range,
+                    "interested": user_detail.interest
+                }
+                r_user = RegisterUser.objects.get(email=user.email)
+                account = DeactivateAccount.objects.get(user=r_user)
+                if account.deactivated:
+                    account.deactivated = False
+                    account.save()
+                    user_detail.deactivated = False
+                    user_detail.save()
+                return Response(
+                    {"Token": token.key, "user_id": reg_usr.id, 'data': Data, 'deactivated': account.deactivated,
+                     "status": HTTP_200_OK})
             else:
                 return Response({"message": serializer.errors, "status": HTTP_400_BAD_REQUEST})
 
@@ -2553,7 +2724,92 @@ class GoogleSignupView(CreateAPIView):
                 user_with_token = user_with_token[0]
                 print(user_with_token)
                 user_data = RegisterUser.objects.get(email=existing_user.email)
-                return Response({"Token": user_with_token.key, "user id": user_data.id, "status": HTTP_200_OK})
+                # user_data = RegisterUser.objects.get(email=user.email)
+                user_detail = UserDetail.objects.get(phone_number=user_data)
+                pic_1 = ''
+                pic_2 = ''
+                pic_3 = ''
+                pic_4 = ''
+                pic_5 = ''
+                pic_6 = ''
+                pic_7 = ''
+                pic_8 = ''
+                pic_9 = ''
+                if user_data.pic_1:
+                    pic_1 = user_data.pic_1.url
+                else:
+                    pic_1 = ''
+                if user_data.pic_2:
+                    pic_2 = user_data.pic_2.url
+                else:
+                    pic_2 = ''
+                if user_data.pic_3:
+                    pic_3 = user_data.pic_3.url
+                else:
+                    pic_3 = ''
+                if user_data.pic_4:
+                    pic_4 = user_data.pic_4.url
+                else:
+                    pic_4 = ''
+                if user_data.pic_5:
+                    pic_5 = user_data.pic_5.url
+                else:
+                    pic_5 = ''
+                if user_data.pic_6:
+                    pic_6 = user_data.pic_6.url
+                else:
+                    pic_6 = ''
+                # if user_data.pic_7:
+                #     pic_7 = user_data.pic_8.url
+                # else:
+                #     pic_7 = ''
+                # if user_data.pic_8:
+                #     pic_8 = user_data.pic_8.url
+                # else:
+                #     pic_8 = ''
+                # if user_data.pic_9:
+                #     pic_9 = user_data.pic_9.url
+                # else:
+                #     pic_9 = ''
+                Data = {
+                    "id": user_data.id,
+                    "email": user_data.email,
+                    "first_name": user_data.first_name,
+                    "last_name": user_data.last_name,
+                    "phone_number": user_data.phone_number,
+                    "gender": user_data.gender,
+                    "date_of_birth": user_data.date_of_birth,
+                    # "job_profile": user_data.job_profile,
+                    # "company_name": user_data.company_name,
+                    # "qualification": user_data.qualification,
+                    # "relationship_status": user_data.relationship_status,
+                    # "interests": user_data.interests,
+                    # "fav_quote": user_data.fav_quote,
+                    "pic_1": pic_1,
+                    "pic_2": pic_2,
+                    "pic_3": pic_3,
+                    "pic_4": pic_4,
+                    "pic_5": pic_5,
+                    "pic_6": pic_6,
+                    # "pic_7": pic_7,
+                    # "pic_8": pic_8,
+                    # "pic_9": pic_9,
+                    "discovery_lat": user_detail.discovery[0],
+                    "discovery_lang": user_detail.discovery[1],
+                    "distance_range": user_detail.distance_range,
+                    "min_age_range": user_detail.min_age_range,
+                    "max_age_range": user_detail.max_age_range,
+                    "interested": user_detail.interest
+                }
+                r_user = RegisterUser.objects.get(email=existing_user.email)
+                account = DeactivateAccount.objects.get(user=r_user)
+                if account.deactivated:
+                    account.deactivated = False
+                    account.save()
+                    user_detail.deactivated = False
+                    user_detail.save()
+                return Response({"Token": user_with_token.key, "user id": user_data.id, 'data': Data,
+                                 'deactivated': account.deactivated, "status": HTTP_200_OK})
         except:
             print('Gmail except ----------------->>>>>>>>>>>', self.request.data)
             # print('>>>>>>>>>>>>>>', self.request.data.POST('email'))
@@ -2581,7 +2837,93 @@ class GoogleSignupView(CreateAPIView):
                     user=reg_usr
                 )
                 token = Token.objects.create(user=user)
-                return Response({"Token": token.key, "user_id": reg_usr.id, "status": HTTP_200_OK})
+                user_data = RegisterUser.objects.get(email=existing_user.email)
+                user_data = RegisterUser.objects.get(email=user.email)
+                user_detail = UserDetail.objects.get(phone_number=user_data)
+                pic_1 = ''
+                pic_2 = ''
+                pic_3 = ''
+                pic_4 = ''
+                pic_5 = ''
+                pic_6 = ''
+                pic_7 = ''
+                pic_8 = ''
+                pic_9 = ''
+                if user_data.pic_1:
+                    pic_1 = user_data.pic_1.url
+                else:
+                    pic_1 = ''
+                if user_data.pic_2:
+                    pic_2 = user_data.pic_2.url
+                else:
+                    pic_2 = ''
+                if user_data.pic_3:
+                    pic_3 = user_data.pic_3.url
+                else:
+                    pic_3 = ''
+                if user_data.pic_4:
+                    pic_4 = user_data.pic_4.url
+                else:
+                    pic_4 = ''
+                if user_data.pic_5:
+                    pic_5 = user_data.pic_5.url
+                else:
+                    pic_5 = ''
+                if user_data.pic_6:
+                    pic_6 = user_data.pic_6.url
+                else:
+                    pic_6 = ''
+                # if user_data.pic_7:
+                #     pic_7 = user_data.pic_8.url
+                # else:
+                #     pic_7 = ''
+                # if user_data.pic_8:
+                #     pic_8 = user_data.pic_8.url
+                # else:
+                #     pic_8 = ''
+                # if user_data.pic_9:
+                #     pic_9 = user_data.pic_9.url
+                # else:
+                #     pic_9 = ''
+                Data = {
+                    "id": user_data.id,
+                    "email": user_data.email,
+                    "first_name": user_data.first_name,
+                    "last_name": user_data.last_name,
+                    "phone_number": user_data.phone_number,
+                    "gender": user_data.gender,
+                    "date_of_birth": user_data.date_of_birth,
+                    # "job_profile": user_data.job_profile,
+                    # "company_name": user_data.company_name,
+                    # "qualification": user_data.qualification,
+                    # "relationship_status": user_data.relationship_status,
+                    # "interests": user_data.interests,
+                    # "fav_quote": user_data.fav_quote,
+                    "pic_1": pic_1,
+                    "pic_2": pic_2,
+                    "pic_3": pic_3,
+                    "pic_4": pic_4,
+                    "pic_5": pic_5,
+                    "pic_6": pic_6,
+                    # "pic_7": pic_7,
+                    # "pic_8": pic_8,
+                    # "pic_9": pic_9,
+                    "discovery_lat": user_detail.discovery[0],
+                    "discovery_lang": user_detail.discovery[1],
+                    "distance_range": user_detail.distance_range,
+                    "min_age_range": user_detail.min_age_range,
+                    "max_age_range": user_detail.max_age_range,
+                    "interested": user_detail.interest
+                }
+                r_user = RegisterUser.objects.get(email=existing_user.email)
+                account = DeactivateAccount.objects.get(user=r_user)
+                if account.deactivated:
+                    account.deactivated = False
+                    account.save()
+                    user_detail.deactivated = False
+                    user_detail.save()
+                return Response({"Token": token.key, "user_id": reg_usr.id, 'data': Data,
+                                 'deactivated': account.deactivated, "status": HTTP_200_OK})
             else:
                 return Response({"message": serializer.errors, "status": HTTP_400_BAD_REQUEST})
 
