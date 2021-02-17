c393e623c70126977d196643b6eb0f8dd18adc2f
choudharyamit26 <ravichoudhary766@gmail.com>
2021-02-17 17:14:07 +0530

    Added super liked user in liked users list

--- chat/consumers.py
+++ chat/consumers.py
@@ -56,17 +56,18 @@ class ChatRoomConsumer(WebsocketConsumer):
                     email = sender.email
                     print(email)
                     first_name = sender.first_name
+                    print(first_name)
                     fcm_token = User.objects.get(email=email).device_token
                     data_message = {"data": {"title": 'first_name',
                                              "body": "text_data_json['message']",
                                              "type": "NewMessage"}}
                     respo = send_to_one(fcm_token, data_message)
                     print(respo)
-                    title = first_name
-                    body = text_data_json['message']
-                    message_type = "newMessage"
-                    respo = send_another(fcm_token, title, body, message_type)
-                    print(respo)
+                    # title = first_name
+                    # body = text_data_json['message']
+                    # message_type = "newMessage"
+                    # respo = send_another(fcm_token, title, body, message_type)
+                    # print(respo)
                 except Exception as e:
                     print('Inside fcm exception',e)
                     pass
