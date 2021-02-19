17855194397cbeb173c983612f9273655b3bbe57
choudharyamit26 <ravichoudhary766@gmail.com>
2021-02-19 16:22:44 +0530

    added unread message count

--- chat/consumers.py
+++ chat/consumers.py
@@ -63,12 +63,12 @@ class ChatRoomConsumer(WebsocketConsumer):
                     print(email)
                     print(first_name)
                     data_message = {
-                        "data": {
+                        # "data": {
                             "body": text_data_json['message'],
                             "title": first_name,
                             "is_image": str(text_data_json['is_image']),
                             "room_id": chat1.id
-                        }
+                        # }
                     }
                     respo = send_to_one(fcm_token, data_message)
                     print(respo)
@@ -113,12 +113,12 @@ class ChatRoomConsumer(WebsocketConsumer):
                     print('FCM TOKEN ', fcm_token)
                     print(email)
                     data_message = {
-                        "data": {
+                        # "data": {
                             "body": text_data_json['message'],
                             "title": first_name,
                             "is_image": str(text_data_json['is_image']),
                             "room_id": chat1.id
-                        }
+                        # }
                     }
                     respo = send_to_one(fcm_token, data_message)
                     print(respo)
@@ -162,12 +162,12 @@ class ChatRoomConsumer(WebsocketConsumer):
                 print(x.id)
                 print(email)
                 data_message = {
-                    "data": {
+                    # "data": {
                         "body": text_data_json['message'],
                         "title": first_name,
                         "is_image": str(text_data_json['is_image']),
                         "room_id": x.id
-                    }
+                    # }
                 }
                 respo = send_to_one(fcm_token, data_message)
                 print(respo)
