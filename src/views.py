ffe5eb4baf3a9f33f79e021bc3f9232b4c073713
choudharyamit26 <ravichoudhary766@gmail.com>
2021-03-03 16:33:31 +0530

    added pop up api's

--- src/views.py
+++ src/views.py
@@ -2942,13 +2942,15 @@ class FacebookSignupApiView(CreateAPIView):
                     print(pic[0])
                     import os
                     print(os.path.abspath("{}.jpg".format(social_id)))
+                    pic_1 = os.path.abspath("{}.jpg".format(social_id))
                     reg_usr = RegisterUser.objects.create(
                         email=email,
                         first_name=name,
                         date_of_birth=dob,
-                        pic_1=pic[0],
+                        pic_1=pic_1,
                         verified=True
                     )
+                    print(reg_usr.pic_1.url)
                     if os.path.exists("{}.jpg".format(social_id)):
                         os.remove("{}.jpg".format(social_id))
                     else:
