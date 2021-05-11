6fcbf692f6f51c8ebb2c1865455befbfc97ffb4a
choudharyamit26 <ravichoudhary766@gmail.com>
2021-05-11 15:42:13 +0530

    added terms and condition api in adminpanel

--- adminpanel/admin.py
+++ adminpanel/admin.py
@@ -1,6 +1,7 @@
-from .models import User, UserNotification
+from .models import User, UserNotification, TermsCondition
 
 from django.contrib import admin
 
 admin.site.register(User)
 admin.site.register(UserNotification)
+admin.site.register(TermsCondition)
