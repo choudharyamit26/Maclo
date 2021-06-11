9b93a357291614bdafcdb6449c87bc32a6b60d84
choudharyamit26 <ravichoudhary766@gmail.com>
2021-06-11 15:13:51 +0530

    added signals

--- src/views.py
+++ src/views.py
@@ -2592,6 +2592,16 @@ class MeetingDetail(APIView):
                  'invitee_last_name': meeting_obj.scheduled_with.last_name, 'time': meeting_obj.meeting_time,
                  'date': meeting_obj.meeting_date, 'description': meeting_obj.description, 'venue': meeting_obj.venue,
                  'status': HTTP_200_OK})
+        elif not meeting_obj.scheduled_by.pic_1 and not meeting_obj.scheduled_with.pic_1:
+            return Response(
+                {'invited_by': meeting_obj.scheduled_by.id, 'invited_by_pic': '',
+                 'invited_by_first_name': meeting_obj.scheduled_by.first_name,
+                 'invited_by_last_name': meeting_obj.scheduled_by.last_name,
+                 'invitee_id': meeting_obj.scheduled_with.id, 'invitee_pic': '',
+                 'invitee_first_name': meeting_obj.scheduled_with.first_name,
+                 'invitee_last_name': meeting_obj.scheduled_with.last_name, 'time': meeting_obj.meeting_time,
+                 'date': meeting_obj.meeting_date, 'description': meeting_obj.description, 'venue': meeting_obj.venue,
+                 'status': HTTP_200_OK})
         elif meeting_obj.scheduled_by.pic_1:
             return Response(
                 {'invited_by': meeting_obj.scheduled_by.id, 'invited_by_pic': '',
