eaefd62b076cb6178e37f571da1e356ccff3945f
choudharyamit26 <ravichoudhary766@gmail.com>
2021-07-21 15:06:34 +0530

    added update meeting api status field

--- maclo/settings.py
+++ maclo/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&fmi_gxw86%5_tm-35_ell!d4rh2e8!bkfon4qq9(xh2d5y&f1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
