5daf2a171ca9db68871d52ff3280151b897c5027
choudharyamit26 <ravichoudhary766@gmail.com>
2021-07-27 18:46:24 +0530

    added update meeting api status field

--- chat/consumers.py
+++ chat/consumers.py
@@ -371,86 +371,87 @@ class ChatRoomConsumer(WebsocketConsumer):
                 "message": event['m'].message,
                 "is_image": event['m'].is_image,
                 "id": event['m'].id,
-                "created_at": str(event['m'].created_at.replace(microsecond=0))
+                "created_at": str(event['m'].created_at.replace(microsecond=0)),
+                "cleared_by": str(event['m'].cleared_by)
                 # 'sender': event['m'],
                 # 'receiver': event['receiver'],
                 # 'message': event['message']
             }))
 
-# class ChatConsumer(WebsocketConsumer):
-#
-#     def fetch_messages(self, data):
-#         messages = get_last_10_messages(data['chatId'])
-#         content = {
-#             'command': 'messages',
-#             'messages': self.messages_to_json(messages)
-#         }
-#         self.send_message(content)
-#
-#     def new_message(self, data):
-#         user_contact = get_user_contact(data['from'])
-#         message = Message.objects.create(
-#             contact=user_contact,
-#             content=data['message'])
-#         current_chat = get_current_chat(data['chatId'])
-#         current_chat.messages.add(message)
-#         current_chat.save()
-#         content = {
-#             'command': 'new_message',
-#             'message': self.message_to_json(message)
-#         }
-#         return self.send_chat_message(content)
-#
-#     def messages_to_json(self, messages):
-#         result = []
-#         for message in messages:
-#             result.append(self.message_to_json(message))
-#         return result
-#
-#     def message_to_json(self, message):
-#         return {
-#             'id': message.id,
-#             'author': message.contact.user.first_name,
-#             'content': message.content,
-#             'timestamp': str(message.timestamp)
-#         }
-#
-#     commands = {
-#         'fetch_messages': fetch_messages,
-#         'new_message': new_message
-#     }
-#
-#     def connect(self):
-#         self.room_name = self.scope['url_route']['kwargs']['room_name']
-#         self.room_group_name = 'chat_%s' % self.room_name
-#         async_to_sync(self.channel_layer.group_add)(
-#             self.room_group_name,
-#             self.channel_name
-#         )
-#         self.accept()
-#
-#     def disconnect(self, close_code):
-#         async_to_sync(self.channel_layer.group_discard)(
-#             self.room_group_name,
-#             self.channel_name
-#         )
-#
-#     def receive(self, text_data):
-#         data = json.loads(text_data)
-#         self.commands[data['command']](self, data)
-#
-#     def send_chat_message(self, message):
-#         async_to_sync(self.channel_layer.group_send)(
-#             self.room_group_name,
-#             {
-#                 'type': 'chat_message',
-#                 'message': message
-#             }
-#         )
-#
-#     def send_message(self, message):
-#         self.send(text_data=json.dumps(message))
-#
-#     def chat_message(self, event):
-#         message = event['message']
-#         self.send(text_data=json.dumps(message))
+        # class ChatConsumer(WebsocketConsumer):
+        #
+        #     def fetch_messages(self, data):
+        #         messages = get_last_10_messages(data['chatId'])
+        #         content = {
+        #             'command': 'messages',
+        #             'messages': self.messages_to_json(messages)
+        #         }
+        #         self.send_message(content)
+        #
+        #     def new_message(self, data):
+        #         user_contact = get_user_contact(data['from'])
+        #         message = Message.objects.create(
+        #             contact=user_contact,
+        #             content=data['message'])
+        #         current_chat = get_current_chat(data['chatId'])
+        #         current_chat.messages.add(message)
+        #         current_chat.save()
+        #         content = {
+        #             'command': 'new_message',
+        #             'message': self.message_to_json(message)
+        #         }
+        #         return self.send_chat_message(content)
+        #
+        #     def messages_to_json(self, messages):
+        #         result = []
+        #         for message in messages:
+        #             result.append(self.message_to_json(message))
+        #         return result
+        #
+        #     def message_to_json(self, message):
+        #         return {
+        #             'id': message.id,
+        #             'author': message.contact.user.first_name,
+        #             'content': message.content,
+        #             'timestamp': str(message.timestamp)
+        #         }
+        #
+        #     commands = {
+        #         'fetch_messages': fetch_messages,
+        #         'new_message': new_message
+        #     }
+        #
+        #     def connect(self):
+        #         self.room_name = self.scope['url_route']['kwargs']['room_name']
+        #         self.room_group_name = 'chat_%s' % self.room_name
+        #         async_to_sync(self.channel_layer.group_add)(
+        #             self.room_group_name,
+        #             self.channel_name
+        #         )
+        #         self.accept()
+        #
+        #     def disconnect(self, close_code):
+        #         async_to_sync(self.channel_layer.group_discard)(
+        #             self.room_group_name,
+        #             self.channel_name
+        #         )
+        #
+        #     def receive(self, text_data):
+        #         data = json.loads(text_data)
+        #         self.commands[data['command']](self, data)
+        #
+        #     def send_chat_message(self, message):
+        #         async_to_sync(self.channel_layer.group_send)(
+        #             self.room_group_name,
+        #             {
+        #                 'type': 'chat_message',
+        #                 'message': message
+        #             }
+        #         )
+        #
+        #     def send_message(self, message):
+        #         self.send(text_data=json.dumps(message))
+        #
+        #     def chat_message(self, event):
+        #         message = event['message']
+        #         self.send(text_data=json.dumps(message))
